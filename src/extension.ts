a131f4c410339a1178fbbc025faf80cf2effbb49
Pranay Prakash <pranay.gp@gmail.com>
2016-12-02 01:03:57 -0600

    Support CSS instead of Files

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,8 @@ import * as vscode from 'vscode';
 
 import * as fs   from 'fs';
 import * as path from 'path';
+const _ = require('lodash');
+const css = require('css');
 
 
 // this method is called when your extension is activated
@@ -10,9 +12,9 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	//console.log('Congratulations, your extension "vscode-file-peek" is now active!');
+	console.log('Congratulations, your extension "vscode-css-peek" is now active!');
 
-   let config = vscode.workspace.getConfiguration('file_peek');
+   let config = vscode.workspace.getConfiguration('css_peek');
    let active_languages       = (config.get('activeLanguages') as Array<string>);
    let search_file_extensions = (config.get('searchFileExtensions') as Array<string>);
 
@@ -40,6 +42,23 @@ export function activate(context: vscode.ExtensionContext) {
 export function deactivate() {
 }
 
+/**
+ * Function that walks a directory recursively and returns all files
+ */
+var walkSync = function(dir, filelist) {
+  var fs = fs || require('fs'),
+      files = fs.readdirSync(dir);
+  filelist = filelist || [];
+  files.forEach(function(file) {
+    if (fs.statSync(dir + file).isDirectory()) {
+      filelist = walkSync(dir + file + '/', filelist);
+    }
+    else {
+      filelist.push(file);
+    }
+  });
+  return filelist;
+};
 
 /**
  * Provide the lookup so we can peek into the files.
@@ -51,39 +70,9 @@ class PeekFileDefinitionProvider implements vscode.DefinitionProvider {
       this.fileSearchExtensions = fileSearchExtensions;
    }
 
-   /**
-    * Return list of potential paths to check
-    * based upon file search extensions for a potential lookup.
-    */
-   getPotentialPaths(lookupPath: string): string[] {
-      let potential_paths: string[] = [lookupPath];
-
-      // Add on list where we just add the file extension directly
-      this.fileSearchExtensions.forEach((extStr) => {
-         potential_paths.push(lookupPath + extStr);
-      });
-
-      // if we have an extension, then try replacing it.
-      let parsed_path = path.parse(lookupPath);
-      if (parsed_path.ext !== "") {
-         this.fileSearchExtensions.forEach((extStr) => {
-            const new_path = path.format({
-               base: parsed_path.name + extStr,
-               dir: parsed_path.dir,
-               ext: extStr,
-               name: parsed_path.name,
-               root: parsed_path.root
-            });
-            potential_paths.push(new_path);
-         });
-      }
-
-      return potential_paths;
-   }
-
-   provideDefinition(document: vscode.TextDocument,
+   async provideDefinition(document: vscode.TextDocument,
                      position: vscode.Position,
-                     token: vscode.CancellationToken): vscode.Definition {
+                     token: vscode.CancellationToken): Promise<vscode.Definition> {
       // todo: make this method operate async
       let working_dir = path.dirname(document.fileName);
       let word        = document.getText(document.getWordRangeAtPosition(position));
@@ -118,16 +107,68 @@ class PeekFileDefinitionProvider implements vscode.DefinitionProvider {
             //console.log("  Full: " + full_path);
 
             // Find all potential paths to check and return the first one found
-            let potential_fnames = this.getPotentialPaths(full_path);
-            //console.log(" potential fnames: ", potential_fnames);
+            // let potential_fnames = this.getPotentialPaths(full_path);
+            // let potential_fnames = this.getPotentialPaths(working_dir);
+            let potential_fnames: any = _(await vscode.workspace.findFiles("**/*.css", "")).map(f => f.fsPath)
+            // let potential_fnames: any = _(await vscode.workspace.findFiles("**/*.css", "")).map(f => f.fsPath).reject(f => /node_modules/ig.test(f) || /bower_components/ig.test(f)).value();
 
-            let found_fname = potential_fnames.find((fname_full) => {
-               //console.log(" checking: ", fname_full);
-               return fs.existsSync(fname_full);
+            //console.log(" potential fnames: ", potential_fnames);
+            let found_fname = potential_fnames.find(file => {
+              const file_text = fs.readFileSync(file, "utf8");
+              let parsed_css = null;
+              try {
+                parsed_css = css.parse(file_text)
+                if(!parsed_css) throw new Error("No CSS ?")
+                if(parsed_css.type !== "stylesheet") throw new Error("CSS isn't a stylesheet")
+                if(!parsed_css.stylesheet.rules) throw new Error("no CSS rules")
+
+                let rule = parsed_css.stylesheet.rules.find(rule => {
+                  return rule.type == "rule" &&  ( _.includes(rule.selectors, "." + word, 0) || _.includes(rule.selectors, "#" + word, 0) ) // TODO: don't generalize class and ID selector
+                })
+
+                if(!rule) throw new Error("CSS rule not found")
+
+                return true;
+
+              } catch (error) {
+                //Error in parsing CSS
+                // position = new vscode.Position(0, 1);
+                // console.log(parsed_css);
+                // console.error(error);
+              }
+              return false;
             });
-            if (found_fname != null) {
+
+            found_fname = found_fname || potential_fnames[0];
+
+            if (found_fname) {
                console.log('found: ' + found_fname);
-               return new vscode.Location(vscode.Uri.file(found_fname), new vscode.Position(0, 1));
+               const file_text = fs.readFileSync(found_fname, "utf8");
+              //  console.log(css.parse("h1{\"color\":\"#000\"}"));
+              let position = null;
+              let parsed_css = null;
+              try {
+                parsed_css = css.parse(file_text)
+                if(!parsed_css) throw new Error("No CSS ?")
+                if(parsed_css.type !== "stylesheet") throw new Error("CSS isn't a stylesheet")
+                if(!parsed_css.stylesheet.rules) throw new Error("no CSS rules")
+
+                let rule = parsed_css.stylesheet.rules.find(rule => {
+                  return rule.type == "rule" &&  ( _.includes(rule.selectors, "." + word, 0) || _.includes(rule.selectors, "#" + word, 0) ) // TODO: don't generalize class and ID selector
+                })
+
+                if(!rule) throw new Error("CSS rule not found")
+
+                position = new vscode.Position(rule.position.start.line, rule.position.start.column);
+
+              } catch (error) {
+                //Error in parsing CSS
+                position = new vscode.Position(0, 1);
+                console.log(parsed_css);
+                // console.error(error);
+              }
+              // console.log(parsed_css);
+               return new vscode.Location(vscode.Uri.file(found_fname), position);
             }
          }
       }
