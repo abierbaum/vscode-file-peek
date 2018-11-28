e886efdbcde68a11f8bd2d4caa5e22e4e1ac212f
ReiMcCl <reibmc@gmail.com>
2018-11-28 00:12:20 +0000

    Added js test case

---
+++ client/test/test_files/example.js
@@ -0,0 +1,13 @@
+class Example {
+	render () {
+		return (
+			<div>
+				<h1 class="test common" id="testID common">heading</h1>
+				<div class="container">
+					{/* <!-- comment --> */}
+					<span id="test-2" />
+				</div>
+			</div>
+		);
+	}
+}
