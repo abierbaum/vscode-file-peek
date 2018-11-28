a18e9138e79d124ce2afc1159c56d5606f98d208
Ray McClain <rmcclain@decibelinsight.com>
2018-11-28 09:38:27 +0000

    Added className to tests

--- client/test/test_files/example.jsx
+++ client/test/test_files/example.jsx
@@ -3,6 +3,7 @@ class Example {
 		return (
 			<div>
 				<h1 class="test common" id="testID common">heading</h1>
+				<h1 className="test common">heading</h1>
 				<div class="container">
 					{/* <!-- comment --> */}
 					<span id="test-2" />
